43cd40916e32ff2408dd18757ec360d13856d4ef
Ranger1986 <pierre6richard7@gmail.com>
2023-11-08 17:45:48 +0100

    grille

--- grille.py
+++ grille.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+class Grille:
+    def __init__(self, dimX: int, dimY:int):
+        self.x=dimX
+        self.y=dimY
+        self.grille=[[0 for x in self.x] for y in self.y]
+    def getCell(self, position):
+        return self.grille[position[0]][position[1]]
+    def setCell(self, position, donnee):
+        self.grille[position[0]][position[1]]=donnee
