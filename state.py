e4c6a360834a8803d216efd4049ed08ae39f0856
Patateon <maximebeldjilali@gmail.com>
2023-11-22 18:03:00 +0100

    State fix

--- state.py
+++ state.py
@@ -1,6 +1,9 @@
-vide = 0
-lowTree = 1
-midTree = 2
-highTree = 3
-fruitTree = 4
-lumber = 5
+from enum import Enum
+
+class State(enum):
+    vide = 0
+    lowTree = 1
+    midTree = 2
+    highTree = 3
+    fruitTree = 4
+    lumber = 5
