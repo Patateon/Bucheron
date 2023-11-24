568a7f232e4731271ab9d1bd63b17d78172ad439
Patateon <maximebeldjilali@gmail.com>
2023-11-24 23:41:49 +0100

    Add method generateAgent and initGame

--- game.py
+++ game.py
@@ -9,7 +9,12 @@ class Game:
         self.nbAgents = nbAgents
         self.nbArbres = nbArbres
         self.arbres = []
+        self.agents = []
+        self.initGame()
+
+    def initGame(self):
         self.generateTree()
+        self.generateAgent()
 
     def generateTree(self):
         nbA = 0
@@ -18,5 +23,15 @@ class Game:
             typeArbre = random.randint(1,4)
             if(self.grille.getCell(newArbre) == 0):
                 self.grille.setCell(newArbre, State(typeArbre))
-                self.arbres.append(newArbre)
+                thatArbre = arbre.Arbre(newArbre[0], newArbre[1], State(typeArbre))
+                self.arbres.append(thatArbre)
+                nbA +=1
+
+    def generateAgent(self):
+        nbA = 0
+        while nbA!=self.nbAgents:
+            newAgent = (random.randint(0,self.grille.y - 1),random.randint(0,self.grille.x - 1))
+            if(self.grille.getCell(newAgent) == 0):
+                self.grille.setCell(newAgent, State.lumber)
+                self.agents.append(newAgent)
                 nbA +=1
