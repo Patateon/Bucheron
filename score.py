bd0137d35a8e680a4e0500b1b15e815ab5e42c03
Patateon <maximebeldjilali@gmail.com>
2023-12-06 14:11:53 +0100

    updateScore

--- score.py
+++ score.py
@@ -4,9 +4,10 @@ class Score():
     def __init__(self):
         self.score = 0
         self.nbScoreBois = 0
+        self.nbFruit = 0
 
     def updateScore(self):
-        self.score += nbScoreBois
+        self.score = nbScoreBois + nbFruit
 
     def getScore(self):
         return self.score
@@ -16,3 +17,9 @@ class Score():
 
     def getNbScoreBois(self):
         return self.nbScoreBois
+
+    def setNbFruit(self, nbFruit):
+        self.nbFruit = nbFruit
+
+    def getNbFruit(self):
+        return self.nbFruit
